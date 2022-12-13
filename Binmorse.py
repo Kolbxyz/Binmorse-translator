5efef2ff00a87378c930ca1ff0daee6b74d6dfef
Kolb <Kolbxyz@outlook.com>
2022-12-13 11:20:14 +0100

    Added hex to alpha & alpha to hex

--- Binmorse.py
+++ Binmorse.py
@@ -1,65 +1,82 @@
 ## Libraries
 import json
+import binascii
 
 ## Variables
-MorseData = json.load(open('MorseData.json', "r"))
-BinaryData = json.load(open('BinaryData.json', "r"))
+morseData = json.load(open('morseData.json', "r"))
+binaryData = json.load(open('binaryData.json', "r"))
 
-Debug = False
+debug = False
 
 ## Functions
-def FromMorseToAlpha():
-    TranslatedCode = ""
-    for c in range(0, len(str.split(ToTranslate, " "))):
-        TranslatedCode += MorseData [str.split(ToTranslate, " ")[c]]
-        if Debug: print(TranslatedCode, c)
-    print("Translating: \"" + ToTranslate + "\" from morse to alphabetic..." + "\n\n" + TranslatedCode + "\n")
+def fromMorseToAlpha():
+    translatedCode = ""
+    for c in range(0, len(str.split(toTranslate, " "))):
+        translatedCode += morseData [str.split(toTranslate, " ")[c]]
+        if debug: print(translatedCode, c)
+    print("Translating: \"" + toTranslate + "\" from morse to alphabetic..." + "\n\n" + translatedCode + "\n")
 
-def FromAlphaToMorse():
-    TranslatedCode = ""
-    for c in range(0, len([*ToTranslate])):
-        for key in MorseData .keys():
-            if MorseData [key] == ToTranslate[c]:
-              TranslatedCode += key + " "
-            if Debug: print(TranslatedCode, key)            
-    TranslatedCode = TranslatedCode[0:len(TranslatedCode)-1]
-    print("Translating: \"" + ToTranslate + "\" from alphabetic to morse..." + "\n\n" + TranslatedCode + "\n")
+def fromAlphaToMorse():
+    translatedCode = ""
+    for c in range(0, len([*toTranslate])):
+        for key in morseData .keys():
+            if morseData [key] == toTranslate[c]:
+              translatedCode += key + " "
+            if debug: print(translatedCode, key)            
+    translatedCode = translatedCode[0:len(translatedCode)-1]
+    print("Translating: \"" + toTranslate + "\" from alphabetic to morse..." + "\n\n" + translatedCode + "\n")
      
-def FromBinaryToAlpha():
-    TranslatedCode = ""
-    for c in range(0, len(str.split(ToTranslate, " "))):
-        TranslatedCode += BinaryData [str.split(ToTranslate, " ")[c]]
-        if Debug: print(TranslatedCode, c)
-    print("Translating: \"" + ToTranslate + "\" from binary to alphabetic..." + "\n\n" + TranslatedCode + "\n")
+def fromBinaryToAlpha():
+    translatedCode = ""
+    for c in range(0, len(str.split(toTranslate, " "))):
+        translatedCode += binaryData [str.split(toTranslate, " ")[c]]
+        if debug: print(translatedCode, c)
+    print("Translating: \"" + toTranslate + "\" from binary to alphabetic..." + "\n\n" + translatedCode + "\n")
 
-def FromAlphaToBinary():
-    TranslatedCode = ""
-    for c in range(0, len([*ToTranslate])):
-        for key in BinaryData .keys():
-            if BinaryData [key] == ToTranslate[c]:
-               TranslatedCode += key + " "
-               if Debug: print(TranslatedCode, key)
+def fromAlphaToBinary():
+    translatedCode = ""
+    for c in range(0, len([*toTranslate])):
+        for key in binaryData .keys():
+            if binaryData [key] == toTranslate[c]:
+               translatedCode += key + " "
+               if debug: print(translatedCode, key)
                 
-    TranslatedCode = TranslatedCode[0:len(TranslatedCode)-1]
-    print("Translating: \"" + ToTranslate + "\" from alphabetic to binary..." + "\n\n" + TranslatedCode + "\n")
+    translatedCode = translatedCode[0:len(translatedCode)-1]
+    print("Translating: \"" + toTranslate + "\" from alphabetic to binary..." + "\n\n" + translatedCode + "\n")
+
+def fromHexToAlpha():
+    translatedCode = str(binascii.unhexlify(toTranslate.encode('utf-8')))
+    translatedCode = translatedCode.replace("b'","")
+    translatedCode = translatedCode.replace("'","")
+    print("Translating: \"" + toTranslate + "\" from hex to alphabetic..." + "\n\n" + translatedCode + "\n")
+
+def fromAlphaToHex():
+    translatedCode = str(binascii.hexlify(toTranslate.encode('utf-8')))
+    translatedCode = translatedCode.replace("b'","")
+    translatedCode = translatedCode.replace("'","")
+    print("Translating: \"" + toTranslate + "\" from alphabetic to hex..." + "\n\n" + translatedCode + "\n")
 
 ## Connectors
 print("~~~~~~~~ Welcome to BINMORSE translator! ~~~~~~~~")
 
 while True:
-    Mode = int(input("1: Morse --> Alphabetic\n2: Alphabetic --> Morse\n3: Binary --> Alphabetic\n4: Alphabetic --> Binary\n\nEnter mode's number:"))
-    print("Selected mode: " + (Mode == 1 and "Morse to alphabetic" or Mode == 2 and "Alphabetic to morse" or Mode == 3 and "Binary to Alphabetic" or "Alphabetic to Binary") + " (" + str(Mode) + ")")
+    mode = int(input("1: Morse --> Alphabetic\n2: Alphabetic --> Morse\n3: Binary --> Alphabetic\n4: Alphabetic --> Binary\n5: Hex --> Alphabetic\n6: Alphabetic --> Hex\n\nEnter mode's number:"))
+    print("Selected mode: " + (mode == 1 and "Morse to alphabetic" or mode == 2 and "Alphabetic to morse" or mode == 3 and "Binary to Alphabetic" or mode == 4 and "Alphabetic to Binary" or mode == 5 and "Alphabetic to Hex" or mode == 6 and "Hex to Alphabetic" ) + " (" + str(mode) + ")")
 
-    ToTranslate = str.lower(str(input("\nString to translate: ")))
-    TranslatedCode = ""
-    if Mode == 1:
-        FromMorseToAlpha()
-    elif Mode == 2:
-        FromAlphaToMorse()
-    elif Mode == 3:
-        FromBinaryToAlpha()
-    elif Mode == 4:
-        FromAlphaToBinary()
+    toTranslate = str.lower(str(input("\nString to translate: ")))
+    translatedCode = ""
+    if mode == 1:
+        fromMorseToAlpha()
+    elif mode == 2:
+        fromAlphaToMorse()
+    elif mode == 3:
+        fromBinaryToAlpha()
+    elif mode == 4:
+        fromAlphaToBinary()
+    elif mode == 5:
+        fromHexToAlpha()
+    elif mode == 6:
+        fromAlphaToHex()
     else:
         exit()
 
